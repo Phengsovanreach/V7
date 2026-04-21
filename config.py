ee44ec2c3c092c2081c0cee51d7f4163b1f479f5
Phengsovanreach <phengsovanreach4@gmail.com>
2026-04-21 23:20:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN missing")
-
-if not WEBHOOK_URL:
-    raise RuntimeError("WEBHOOK_URL missing")
